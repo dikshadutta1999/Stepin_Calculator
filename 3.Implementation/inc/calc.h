c0a2ab56969493ab32f3fc08ba29c3914aab76ec
dikshadutta1999 <48317679+dikshadutta1999@users.noreply.github.com>
2021-09-08 16:52:31 +0530

    Add files via upload

---
+++ 3.Implementation/inc/calc.h
@@ -0,0 +1,34 @@
+/**
+	* @file calc.h
+	*/
+#ifndef __CALC_H__
+#define __CALC_H__
+
+/**
+	* Calculates the sum of two integer numbers
+	* @param[in] num1 first integer for sum
+	* @param[in] num2 seconf integer for sum
+	* @return Sum of num1 & num1
+	* @note Any note for the function
+    * @param[in] num4 first integer for division
+	* @param[in] num5 seconf integer for division
+	* @return Division of num4 & num5
+	* @note Any note for the function
+    * @param[in] num7 first integer for Subtraction
+	* @param[in] num8 seconf integer for Subtraction
+	* @return Subtraction of num7 & num8
+	* @note Any note for the function
+    * @param[in] num10 first integer for Multiplication
+	* @param[in] num11 seconf integer for Multiplication
+	* @return Multiplication of num1 & num1
+	* @note Any note for the function
+	*/
+
+
+int test_main(void);
+int sum(int num1,int num2);
+int divis(int num4, int num5);
+int sub(int num7, int num8);
+int mul(int num10,int num11);
+
+#endif /*__CALC_H__*/
